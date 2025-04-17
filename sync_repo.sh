72f115c9739b68a3c221f04f063687fe0b38c2af
layman127 <biock2105@gmail.com>
2025-04-18 00:03:58 +0200

    build: add sync script

---
+++ sync_repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Параметры
+REPO_URL="https://github.com/SoulGarage/api.git"
+CLONE_DIR="./backend"
+DEST_DIR="../"
+FILES=("README.md" ".env.example" ".github" ".husky" ".vscode" "prisma" "docker-compose.yml" ".prettierrc" "eslint.config.mjs")
+
+cd ./apps
+
+rm -rf ./backend
+# Клонирование репозитория
+if git clone --branch dev "$REPO_URL" "$CLONE_DIR"; then
+  echo "Repository cloned successfully"
+else
+  echo "Failed to clone repository"
+  exit 1
+fi
+
+# Цикл по файлам и перемещение каждого файла
+for FILE in "${FILES[@]}"; do
+  mv "$CLONE_DIR/$FILE" "$DEST_DIR/"
+done
